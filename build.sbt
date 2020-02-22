982bc2fe773abcc3d212e7a0c465e67049a3ab71
Michele Vascellari <michele.vascellari@gmail.com>
2020-02-22 12:33:48 +0000

    Add functions

--- build.sbt
+++ build.sbt
@@ -8,6 +8,15 @@ scalaVersion := "2.12.10"
 libraryDependencies ++= Seq(
   "org.apache.kafka" %% "kafka" % "2.1.0",
   "com.typesafe.play" %% "play-json" % "2.8.1",
-  "org.slf4j" % "slf4j-jdk14" % "1.7.30"
+  // "org.slf4j" % "slf4j-jdk14" % "1.7.30",
+  "org.slf4j" % "slf4j-api" % "1.7.30",
+  "org.slf4j" % "slf4j-simple" % "1.7.30",
+  "com.github.nscala-time" %% "nscala-time" % "2.22.0"
 )
 
+
+//https://stackoverflow.com/a/39058507
+assemblyMergeStrategy in assembly := {
+ case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+ case x => MergeStrategy.first
+}
--- src/main/scala/com/mvasce/Consumer.scala
+++ src/main/scala/com/mvasce/Consumer.scala
@@ -9,39 +9,88 @@ import play.api.libs.json.{JsValue,Json}
 import org.apache.kafka.common.serialization.Deserializer
 
 import scala.collection.JavaConverters._
+import com.github.nscala_time.time.Imports.DateTime
+import org.slf4j.LoggerFactory
+
 
 object Consumer {
+
+  val BOOTSTRAP_SERVER = "localhost:9092"
+  val TOPIC = "event"
+
+  val logger = LoggerFactory.getLogger(this.getClass)
+
   def main(args: Array[String]): Unit = {
-    consumeFromKafka("event")
+    val (topic, bootstrap_server) = args.length match {
+      case 2 => (args(0), args(1))
+      case 1 => (args(0), BOOTSTRAP_SERVER)
+      case 0 => (TOPIC, BOOTSTRAP_SERVER)
+    }
+    logger.info(s"Connecting to $bootstrap_server topic: $topic")
+    consumeFromKafka(topic, bootstrap_server)
   }
-  
-  def consumeFromKafka(topic: String) = {
-    val props = new Properties()
-    props.put("bootstrap.servers", "localhost:9092")
-    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
-    props.put("auto.offset.reset", "earliest")
-    props.put("group.id", "consumer-group")
-//    props.put("key.deserializer", classOf[StringDeserializer])
-    props.put("value.deserializer", classOf[CustomTypeDeserializer])
+
+  /**
+    * Consumer messages from Kafka
+    *
+    * @param topic : name of the topic
+    * @param bootstrap_server : name of the bootstrap servers
+    */
+  def consumeFromKafka(topic: String, bootstrap_server: String = "localhost:9092") = {
+    val props = getKafkaProperties(topic, bootstrap_server)
     val consumer: KafkaConsumer[String, String] = new KafkaConsumer[String, String](props)
     consumer.subscribe(util.Arrays.asList(topic))
     while (true) {
       val record = consumer.poll(1000).asScala
       for (data <- record.iterator)
         println(data.value())
-        consumer.commitAsync()
+      consumer.commitAsync()
     }
   }
 
+  /**
+    * Get kafka properties object
+    *
+    * @param topic
+    * @param bootstrap_server
+    * @return Kafka properties object
+    */
+  def getKafkaProperties(topic: String, bootstrap_server: String): Properties = {
+    val props = new Properties()
+    props.put("bootstrap.servers", bootstrap_server)
+    props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer")
+    props.put("auto.offset.reset", "earliest")
+    props.put("group.id", "consumer-group")
+    // props.put("value.deserializer", classOf[EventDeserializer])
+    props
+  }
+  
+  /**
+    * Event data type
+    *
+    * @param timestamp : timestamp string using iso
+    * @param event_type : type of event
+    * @param match_id : ID of the match
+    */
   case class Event(timestamp: String, event_type: String, match_id: Int)
+  {
+    val ts: DateTime = DateTime.parse(timestamp)
+
+    /**
+      * Get the timestamp in milliseconds
+      *
+      * @return timestamp in Unix milliseconds
+      */
+    def getCreationTime(): Long = {
+      ts.getMillis
+    }
+  }
 
   implicit val eventFormat = Json.format[Event]
 
 
-  class CustomTypeDeserializer extends Deserializer[Event] {
-//    private val gson: Gson = new Gson()
-
+  class EventDeserializer extends Deserializer[Event] {
     override def deserialize(topic: String, bytes: Array[Byte]): Event = {
       val json: JsValue = Json.parse(bytes)
       Json.fromJson[Event](json).get
